3dea559c28d493c3fa81c09c886acf64a4c8591b
Edwin Arellano <edwinarellano77@gmail.com>
2026-08-12 21:03:45 -0400

    EA: COMMIT 05/Paquete Servicio

---
+++ src/com/krakedev/financiero/servicios/Banco.java
@@ -0,0 +1,20 @@
+package com.krakedev.financiero.servicios;
+
+public class Banco {
+
+	private int ultimoCodigo=1000;
+	
+	public Banco() {
+		
+	}
+
+	public int getUltimoCodigo() {
+		return ultimoCodigo;
+	}
+
+	public void setUltimoCodigo(int ultimoCodigo) {
+		this.ultimoCodigo = ultimoCodigo;
+	}
+	
+	
+}
